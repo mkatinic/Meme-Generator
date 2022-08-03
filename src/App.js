94d2d5b0eb4b88268a751ace6cd096e3b35e43e7
mkatinic <mwkatinic@gmail.com>
2022-08-03 09:14:48 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Meme'
+
+
+export default function(){
+  return(
+    <div>
+      <Header />
+      <Main />
+    </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img className='header--logo' src='./images/trollface.png'></img>
+            <h2 className='header--title'>Meme Generator</h2>
+        </header>
+    )
+}
